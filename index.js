a09f4b06e48f0643016679b44e2a89ea9fcfc2c3
hahaCoder <1306846969@qq.com>
2022-11-01 11:24:21 +0800

    feat(monkey): a friendly cli tool called monkey-cli
    welcome to monkey-cli tool~
    
    BREAKING CHANGE: monkey-cli tool 1.0.0 started

---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+
+const inquirer = require('inquirer')
+
+inquirer.prompt([
+  {
+    type: 'input', //type： input, number, confirm, list, checkbox ... 
+    name: 'name', // key 名
+    message: 'Your name', // 提示信息
+    default: 'my-node-cli' // 默认值
+  }
+]).then(answers => {
+  // 打印互用输入结果
+  console.log(answers)
+})
